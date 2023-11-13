88905177d44244d18d4315a95ba017cbebd0dc87
Esteban Cabrera <86165162+EstebanCabreraArbizu@users.noreply.github.com>
2023-11-13 17:04:43 -0500

    Update app.py without model

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask import Response
-import cv2
-import mediapipe as mp
-
-
-mp_holistic  = mp.solutions.holistic #holistic model
+import cv2                            #importación de cv2 de openc
+import numpy as np                    #Importación de los arreglos numpy
+import os                             #Importación de funciones con los comandos y PATH's
+from matplotlib import pyplot as plt  #Grafica las imagenes capturadas de OpenCV
+import time                           #Para usar la función sleep para tener el tiempo de detectar los gestos
+import mediapipe as mp                #para las detecciones del rostro, torso y mano
+mp_holistic = mp.solutions.holistic #holistic model
 mp_drawing = mp.solutions.drawing_utils #utilidades de dibujo
+
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.5
+
 
 def mediapipe_detection(image,model):
     image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB) # convierte de BGR a RGB
@@ -29,13 +38,28 @@ def draw_styled_landmarks(image,results): #dibuja los landmarks refinados
     mp_drawing.draw_landmarks(image,results.left_hand_landmarks,mp_holistic.HAND_CONNECTIONS,mp_drawing.DrawingSpec(color=(80,22,76),thickness=2,circle_radius=4),mp_drawing.DrawingSpec(color=(121,44,250),thickness=2,circle_radius=2)) #dibuja las conecciones de la mano izquierda
     mp_drawing.draw_landmarks(image,results.right_hand_landmarks,mp_holistic.HAND_CONNECTIONS,mp_drawing.DrawingSpec(color=(254,117,66),thickness=2,circle_radius=4),mp_drawing.DrawingSpec(color=(245,66,230),thickness=2,circle_radius=2)) #dibuja las conecciones de la mano derecha 
 
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
 
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
+
+
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
 
 app = Flask(__name__)
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-#Detección de caras en formato de cascada
-face_detector = cv2.CascadeClassifier(cv2.data.haarcascades +
-     "haarcascade_frontalface_default.xml")
+
 def generate():
     with mp_holistic.Holistic(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as holistic:
         while True:
@@ -46,6 +70,33 @@ def generate():
                 #print(results)
                 #dibuja  los landmarks
                 draw_styled_landmarks(image,results)
+                # 2. Prediction logic
+                keypoints = extract_keypoints(results)
+                sequence.append(keypoints)
+                sequence = sequence[-30:]
+
+                if len(sequence) == 30:
+                    res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                    print(actions[np.argmax(res)])
+                    predictions.append(np.argmax(res))
+                # 3. Viz logic
+                    if np.unique(predictions[-10:])[0] == np.argmax(res):
+                        if res[np.argmax(res)] > threshold:
+
+                            if len(sentence) > 0:
+                                if actions[np.argmax(res)] != sentence[-1]:
+                                    sentence.append(actions[np.argmax(res)])
+                            else:
+                                sentence.append(actions[np.argmax(res)])
+                    if len(sentence) > 5:
+                        sentence = sentence[-5:]
+                    # Viz probabilities
+                    image = prob_viz(res, actions, image, colors)
+
+                cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
+                cv2.putText(image, ' '.join(sentence), (3, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
                 #La imagen obtenida se convierte en una imagen jpg
                 (flag, encodedImage) = cv2.imencode(".jpg", image)
                 # En caso de no poder renderizar el frame pasa al siguiente
